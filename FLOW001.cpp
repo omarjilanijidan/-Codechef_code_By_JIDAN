c49fc3a6a4fc1a2449abbdd4994003bc19b10d13
MD Omar Jilani Jidan <66664781+omarjilanijidan@users.noreply.github.com>
2022-05-14 12:56:37 +0600

    Add files via upload

---
+++ FLOW001.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int x;
+    cin>>x;
+    while(x-->0)
+    {
+        int a,b;
+        cin>>a>>b;
+        cout<<a+b<<endl;
+    }
+    return 0;
+}
